10de886707d1e7eda4964b8b3cd21368a61ac0f9
opaotti <95851107+opaotti@users.noreply.github.com>
2023-02-11 12:14:40 +0100

    hm

---
+++ My project/Assets/Scripts/Controller.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public TMP_Text GnomeCurrencyText;
+
+
+
+
+    void Start()
+    {
+        GnomeCurrencyText.text = "0 Gnomes";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+}
